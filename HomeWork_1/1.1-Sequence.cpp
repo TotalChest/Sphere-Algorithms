fc84d0677a06daadbb0cb6666fc901b0b419c193
totalchest <13sergey78@mail.ru>
2020-02-28 16:44:05 +0300

    1.2 Done

---
+++ HomeWork_1/1.1-Sequence.cpp
@@ -0,0 +1,11 @@
+#include <istream>
+
+int main(void)
+{
+
+
+
+
+	
+	return 0;
+}
---
+++ HomeWork_1/1.2-Merge.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+
+int main(void)
+{
+	int n, cur_1 = 0, cur_2 = 0, flag = 1;
+	unsigned long count = 0, temp;
+	std::cin >> n;
+
+	unsigned long mas[n];
+	unsigned long res[2*n];
+	for (int i = 0; i < n; i++)
+		std::cin >> mas[i];
+
+	std::cin >> temp;
+	for (int i = 0; i < 2*n; i++)
+	{	
+		if(cur_1 < n && cur_2 < n)
+		{
+			if (temp < mas[cur_1])
+			{
+				if(flag == 1)
+				{
+					count += temp;
+					flag = 0;
+				}
+				else
+					flag = 1;
+				cur_2++;
+				if(cur_2 < n)
+					std::cin >> temp;
+			}
+			else
+			{	
+				if(flag == 1)
+				{
+					count += mas[cur_1];
+					flag = 0;
+				}
+				else
+					flag = 1;
+				cur_1++;
+			} 
+		}
+		else
+		{
+			if(cur_2 < n)
+			{
+				if(flag == 1)
+				{
+					count += temp;
+					flag = 0;
+				}
+				else
+					flag = 1;
+				cur_2++;
+				if(cur_2 < n)
+					std::cin >> temp;
+			}
+			else
+			{
+				if(flag == 1)
+				{
+					count += mas[cur_1];
+					flag = 0;
+				}
+				else
+					flag = 1;
+				cur_1++;
+			}
+		}
+	}
+
+	std::cout << count%1000000000;
+
+
+	return 0;
+}

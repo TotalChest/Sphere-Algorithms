13e9865e800a1ef740529ecc6a09dd66b3edf8d7
totalchest <13sergey78@mail.ru>
2020-02-29 16:53:39 +0300

    1.1 Done

--- HomeWork_1/1.1-Sequence.cpp
+++ HomeWork_1/1.1-Sequence.cpp
@@ -2,10 +2,25 @@
 
 int main(void)
 {
+	unsigned int N;
+	unsigned long long K, L;
 
+	std::cin >> N >> K >> L;
 
+	for(unsigned long i = K; i <= L; i++)
+	{
+		unsigned long j = i - 1;
+		unsigned long M = N;
+		while(j != 0)
+		{
+			if(j >= 1<<(M-1))
+				j -= ((1<<(M-1))-1);
+			M--;
+			j--;
+		}
 
-
+		std::cout << (char)((unsigned int)'a' + M - 1);
+	}		
 	
 	return 0;
 }

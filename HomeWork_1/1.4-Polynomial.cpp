d270adffd4c339f5b85d46f12cc8785b3461d972
totalchest <13sergey78@mail.ru>
2020-02-29 13:12:35 +0300

    1.4 Done

--- HomeWork_1/1.4-Polynomial.cpp
+++ HomeWork_1/1.4-Polynomial.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 
-int main(void)
+int main()
 {
+	unsigned long N, M, MOD, res, x;
+	std::cin >> N >> M >> MOD;
 
+	unsigned long k[N+1];
 
+	for(unsigned long i = 0; i < N + 1; i++)
+	{	
+		std::cin >> x;
+		k[i] = x % MOD;
+	}
 
+	for(unsigned long j = 0; j < M; j++)
+	{
+		std::cin >> x;
+		x = x % MOD; 
+		res = k[0];
 
-	
+		for(unsigned long i = 0; i < N; i++)
+		{
+			res = (res * x + k[i+1]) % MOD;
+		}
+
+		std::cout << res << std::endl;
+	}
+
 	return 0;
 }

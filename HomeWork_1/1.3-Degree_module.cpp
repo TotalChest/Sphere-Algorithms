61ab7b752b8973c6f2a511b410f4e491b0fc852e
totalchest <13sergey78@mail.ru>
2020-02-29 11:43:34 +0300

    1.3 Done

--- HomeWork_1/1.3-Degree_module.cpp
+++ HomeWork_1/1.3-Degree_module.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
-long pow(long long X, long long P, long M)
+unsigned long long pow(unsigned long long X, unsigned long long P, unsigned long long M)
 {
-	if(P == 0) return (long)1;
-	if(P%2) return (long)(pow(X, P - 1, M)*X) % M;
-	long y = pow(X, P/2, M);
-	return y*y % M;
+	if(P == 0) return (unsigned long long)1;
+	if(P%2) return (unsigned long long)((pow(X, P - 1, M) * (X % M)) % M);
+	unsigned long long y = pow(X, P/2, M);
+	return (unsigned long long)((y*y) % M);
 }
 
 int main()
 {
-	long long X, P, c = 1;
-	long M;
+	unsigned long long X, P, M;
 	std::cin >> X;
 	std::cin >> P;
 	std::cin >> M;

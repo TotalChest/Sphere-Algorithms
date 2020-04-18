a947b048df61361c3a105cf55b6d4a817b114f9b
totalchest <13sergey78@mail.ru>
2020-04-19 00:02:22 +0300

    Add 6.3

--- HomeWork_6/6.3-Intersection_of_sets.cpp
+++ HomeWork_6/6.3-Intersection_of_sets.cpp
@@ -3,19 +3,21 @@
 #include <unordered_map>
 using namespace std;
 
+vector<unsigned short> freq(1000, 0);
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int N, M, curr, max = 0;
+	unsigned short N, M, max = 0;
+	int curr;
 	cin >> N >> M;
-	unordered_map<int, vector<int>> MAP;
-	vector<int> freq(N, 0);
-	for (int j = 0; j < M; ++j) {
+	unordered_map<int, vector<unsigned short>> MAP;
+	for (unsigned short j = 0; j < M; ++j) {
 		cin >> curr;
 		MAP[curr].push_back(0);
 	}
-	for (int i = 1; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
+	for (unsigned short i = 1; i < N; ++i) {
+		for (unsigned short j = 0; j < M; ++j) {
 			cin >> curr;
 
 			if(MAP.find(curr) == MAP.end())
@@ -26,7 +28,7 @@ int main() {
 				MAP[curr].push_back(i);
 			}
 		}
-		for (int k = 0; k < i; ++k) {
+		for (unsigned short k = 0; k < i; ++k) {
 			if(freq[k] > max)
 				max = freq[k];
 			freq[k] = 0;

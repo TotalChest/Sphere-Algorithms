73dcffe9d589046b54848efe44eb1b5513c7cc6e
totalchest <13sergey78@mail.ru>
2020-05-15 00:17:06 +0300

    8.4 done

--- HomeWork_8/8.4-Speleologists.cpp
+++ HomeWork_8/8.4-Speleologists.cpp
@@ -5,10 +5,12 @@
 #include <queue>
 using namespace std;
 
+long long inf = 1e12;
 long long N, M, K, S, F;
 
-vector<long long> dist(long long src, map<long long, vector<pair<long long, long long>>> &edges) {
-	vector<long long> d(N, -1);
+vector<long long> dist(long long src, map<long long,
+	                   vector<pair<long long, long long>>> &edges) {
+	vector<long long> d(N, inf);
 	vector<long long> color(N, 0);
 	queue<long long> Q;
 
@@ -25,7 +27,7 @@ vector<long long> dist(long long src, map<long long, vector<pair<long long, long
 		for(auto elem: tmp) {
 			if(color[elem.second] == 0)
 				Q.push(elem.second);
-			if (d[elem.second] == -1 || d[elem.second] > d[curr] + elem.first) {
+			if (d[elem.second] > d[curr] + elem.first) {
 				d[elem.second] = d[curr] + elem.first;
 			}
 		}
@@ -49,22 +51,21 @@ int main() {
 		cin >> s >> d >> w;
 		--s;
 		--d;
-		if (s == d)
-			continue;
+
 		edges[s].push_back(make_pair(w,d));
 		edges[d].push_back(make_pair(w,s));
 	}
 	cin >> S >> F; --S; --F;
 	vector<long long> d = dist(F, edges);
-	if (d[S] == -1) {
+	if (d[S] == inf) {
 		cout << -1;
 		return 0;
 	}
-	long long exit = d[S], min = 10000000000;;
+	long long exit = d[S], min = inf;;
 	for (int i = 0; i < K; ++i)
 		if (d[f[i]] < min)
 			min = d[f[i]];
-	if (min < exit)
+	if (min <= exit)
 		cout << -1;
 	else
 		cout << exit;
